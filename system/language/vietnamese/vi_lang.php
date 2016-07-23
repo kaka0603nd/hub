18a4c21868ea063212a5b0a6e5569c741d9d17fe
kaka0603nd <kaka0603nd@gmail.com>
2016-07-23 21:13:36 +0700

    hello

---
+++ system/language/vietnamese/vi_lang.php
@@ -0,0 +1,10 @@
+<?php
+    # Header
+    $lang['username']   	=   "Tên user";
+    $lang['password']		=   "Mật khẩu";
+    $lang['passconf']		=   "Xác nhận Mật khẩu";
+    $lang['matches']		=   "Trường '%s' không khớp với trường '%s'.";
+    $lang['required']   	=   "Trường '%s' là bắt buộc.";
+    $lang['invalid-email']  =   "Email này không hợp lệ.";
+
+    
